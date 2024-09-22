66acc7baed5dbf6390ce152b5ea4bf67f4a82a1f
MoLineTy19 <drcomprf1@gmail.com>
2024-09-22 13:30:59 +0300

    Переписал кнопочки, т.к не было рационально

--- UI/main_window.py
+++ UI/main_window.py
@@ -71,87 +71,65 @@ class TaoApp(QWidget):
 
         inner_layout = self.create_header_info()
 
-        # меню
-        layout = QHBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)  # Убираем отступы
-        layout.setSpacing(0)
-
-        menu = QPushButton()
+        menu = QPushButton(self)
         menu.setIcon(QIcon("resources/menu.png"))
         menu.setIconSize(QSize(32, 32))  # Устанавливаем размер иконки
         menu.setFixedSize(40, 40)  # Устанавливаем фиксированный размер кнопки
         menu.setStyleSheet(button_style)
 
-        layout.addWidget(menu, alignment=Qt.AlignmentFlag.AlignLeft)
-
-        layout1 = self.crete_control_window()
-
-        header_layout.addLayout(layout, stretch=1)
-        header_layout.addLayout(inner_layout, stretch=1)
-        header_layout.addLayout(layout1, stretch=1)
+        header_layout.addLayout(inner_layout)
+        self.crete_control_window()
 
         return header_layout
 
     def crete_control_window(self):
-        # управление окном
-        layout1 = QHBoxLayout()
-        layout1.setContentsMargins(0, 0, 0, 0)  # Убираем отступы
-        layout1.setSpacing(0)  # Убираем отступы между кнопками
-
-        close = QPushButton()
-        close.setIcon(QIcon("resources/close.png"))
-        close.setIconSize(QSize(32, 32))
-        close.setFixedSize(40, 40)
-
-        maximize = QPushButton()
-        maximize.setIcon(QIcon("resources/maximize.png"))
-        maximize.setIconSize(QSize(32, 32))
-        maximize.setFixedSize(40, 40)
-
-        wrap = QPushButton()
-        wrap.setIcon(QIcon("resources/wrap.png"))
-        wrap.setIconSize(QSize(32, 32))
-        wrap.setFixedSize(40, 40)
-
-        close.setStyleSheet(button_style)
-        maximize.setStyleSheet(button_style)
+        wrap = QPushButton(self)
         wrap.setStyleSheet(button_style)
+        wrap.setIcon(QIcon("resources/wrap.png"))
+        wrap.setIconSize(QSize(40, 40))
+        wrap.setFixedSize(40, 40)
+        wrap.move(1160, 0)
 
-        # Добавляем кнопки в layout1 с выравниванием по правому краю
-        layout1.addWidget(wrap, alignment=Qt.AlignmentFlag.AlignRight, stretch=1)
-        layout1.addWidget(maximize, alignment=Qt.AlignmentFlag.AlignRight, stretch=0)
-        layout1.addWidget(close, alignment=Qt.AlignmentFlag.AlignRight, stretch=0)
+        maximize = QPushButton(self)
+        maximize.setStyleSheet(button_style)
+        maximize.setIcon(QIcon("resources/maximize.png"))
+        maximize.setIconSize(QSize(40, 40))
+        maximize.setFixedSize(40, 40)
+        maximize.move(1200, 0)
 
-        # Применение стиля к кнопкам
-
-        return layout1
+        close = QPushButton(self)
+        close.setStyleSheet(button_style)
+        close.setIcon(QIcon("resources/close.png"))
+        close.setIconSize(QSize(40, 40))
+        close.setFixedSize(40, 40)
+        close.move(1240, 0)
 
     def create_header_info(self):
         # Внутренний QHBoxLayout
         inner_layout = QHBoxLayout()
         inner_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
         inner_layout.setContentsMargins(0, 0, 0, 0)
-        inner_layout.setSpacing(10)  # Установите нужное расстояние между элементами
+        inner_layout.setSpacing(10)
 
         # Иконка
         icon = QLabel()
-        pixmap = QPixmap("logo.jpg")  # Замените на путь к вашему изображению
+        pixmap = QPixmap("logo.jpg")
         icon.setPixmap(pixmap)
-        icon.setMaximumHeight(40)  # Ограничиваем высоту иконки
+        icon.setMaximumHeight(40)
         icon.setStyleSheet('QLabel { background-color: transparent; }')
 
         # Название плеера
-        name_player = QLabel('<span style="color: red;">Tao</span> <span style="color: white;">Player</span>')
-        name_player.setStyleSheet(default_text + "font-size: 20px; font-weight: bold;")  # Жирный шрифт
+        name_player = QLabel('<span style="color: white;">Tao</span> <span style="color: #BD321D;">Player</span>', self)
+        name_player.setStyleSheet(default_text + "font-size: 16px; font-weight: bold;")
+        name_player.move(530, 5)
 
         # Версия плеера
-        version = QLabel("v1.0")
-        version.setStyleSheet(default_text + "font-size: 16px; font-weight: bold;")  # Жирный шрифт
+        version = QLabel("ver. 0.1 alpha", self)
+        version.setStyleSheet(default_text + "font-size: 14px; font-weight: regular; color: white;")
+        version.move(668, 4)
 
         # Добавляем элементы во внутренний layout
-        inner_layout.addWidget(name_player, alignment=Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignRight, stretch=1)
-        inner_layout.addWidget(icon, alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter, stretch=0)
-        inner_layout.addWidget(version, alignment=Qt.AlignmentFlag.AlignVCenter, stretch=1)
+        inner_layout.addWidget(icon, alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
         return inner_layout
 
     def create_label(self, text, height):
